89b20a6e2eed3bfc1cb838f65a2b23ad4665f7d0
wonje,song <dev@igaworks.om>
2015-06-25 17:19:53 +0900

    init.

---
+++ AdBrix.framework/Versions/A/Headers/AdBrix.h
@@ -0,0 +1,106 @@
+//
+//  AdBrix.h
+//  AdBrixLib
+//
+//  Created by wonje,song on 2015. 5. 21..
+//  Copyright (c) 2015년 wonje,song. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, AdBrixCustomCohortType)
+{
+    AdBrixCustomCohort_1 = 1,
+    AdBrixCustomCohort_2 = 2,
+    AdBrixCustomCohort_3 = 3
+};
+
+typedef NS_ENUM(NSInteger, AdBrixCurrencyType)
+{
+    AdBrixCurrencyKRW = 1,
+    AdBrixCurrencyUSD = 2,
+    AdBrixCurrencyJPY = 3,
+    AdBrixCurrencyEUR = 4,
+    AdBrixCurrencyGBP = 5,
+    AdBrixCurrencyCHY = 6,
+    AdBrixCurrencyTWD = 7,
+    AdBrixCurrencyHKD = 8
+};
+
+@interface AdBrix : NSObject
+
+/*!
+ @abstract
+ singleton AdBrix 객체를 반환한다.
+ */
++ (AdBrix *)shared;
+
+/*!
+ @abstract
+ first time experience의 Activity에 해당할때 호출한다.
+ 
+ @param activityName              activity name.
+ */
++ (void)firstTimeExperience:(NSString *)activityName;
+
+
+/*!
+ @abstract
+ first time experience의 Activity에 해당할때 호출한다.
+ 
+ @param activityName              activity name.
+ @param param                     parameter.
+ */
++ (void)firstTimeExperience:(NSString *)activityName param:(NSString *)param;
+
+/*!
+ @abstract
+ retension의 Activity에 해당할때 호출한다.
+ 
+ @param activityName              activity name.
+ */
++ (void)retention:(NSString *)activityName;
+
+/*!
+ @abstract
+ retension의 Activity에 해당할때 호출한다.
+ 
+ @param activityName              activity name.
+ @param param                     parameter.
+ */
++ (void)retention:(NSString *)activityName param:(NSString *)param;
+
+
+/*!
+ @abstract
+ buy의 Activity에 해당할때 호출한다.
+ 
+ @param activityName              activity name.
+ */
++ (void)buy:(NSString *)activityName;
+
+/*!
+ @abstract
+ buy의 Activity에 해당할때 호출한다.
+ 
+ @param activityName              activity name.
+ @param param                     parameter.
+ */
++ (void)buy:(NSString *)activityName param:(NSString *)param;
+
+
++ (void)showViralCPINotice:(UIViewController *)viewController;
+
+/*!
+ @abstract
+ cohort 분석시 호출한다.
+ 
+ @param customCohortType          cohort type : AdBrixCustomCohortType
+ @param filterName                filter Name
+ */
++ (void)setCustomCohort:(AdBrixCustomCohortType)customCohortType filterName:(NSString *)filterName;
+
+
+@end
---
+++ AdBrix.podspec
@@ -0,0 +1,24 @@
+Pod::Spec.new do |s|
+s.name         = "AdBrix"
+s.version      = "1.0.0"
+s.summary      = "AdBrix.framework"
+s.homepage     = "https://github.com/IGAWorksDev/AdBrix-iOS"
+
+s.license      = {
+:type => 'Commercial',
+:text => <<-LICENSE
+All text and design is copyright 2006-2015 igaworks, Inc.
+
+All rights reserved.
+
+https://github.com/IGAWorksDev/AdBrix-iOS
+LICENSE
+}
+
+s.platform = :ios, '6.0'
+s.author       = { "wonje,song" => "wonje@igaworks.com" }
+s.source       = { :git => "https://github.com/IGAWorksDev/AdBrix-iOS.git", :tag => "#{s.version}" }
+s.ios.vendored_frameworks = 'AdBrix.framework'
+s.libraries = 'xml2'
+s.xcconfig     = { 'HEADER_SEARCH_PATHS' => '"$(SDKROOT)/usr/include/libxml2"' }
+end
